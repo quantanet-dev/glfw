defd824fd17df6c53d9ba5b3d3fa807671e687e7
Quantanet.Dev <quantanet.dev@gmail.com>
2024-12-07 17:14:35 -0500

    added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,15 @@
+project glfw
+	kind "StaticLib"
+	langauge "C"
+	staticruntime "Off"
+	warnings "Off"
+
+	targetdir("bin/" .. outputDir .. "/%{prj.name}")
+    objdir("bin-obj/" .. outputDir .. "/%{prj.name}")
+
+	files {
+		"src/**.c",
+		"src/**h",
+		"src/**.m",
+		"include/GLFW/**.h"
+	}
